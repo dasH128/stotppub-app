7f7c2003afd71f5272f01839a80fc13a4d60ab79
Dash <rojas71421@gmail.com>
2023-04-04 06:46:16 +0100

    init app

---
+++ android/app/src/main/kotlin/com/example/stotppub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stotppub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:stotppub/src/core/presentacion/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
